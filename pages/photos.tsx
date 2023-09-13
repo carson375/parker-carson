5b858a5e127b8cecb957257c386c28151c4c770c
carson375 <carson.375@osu.edu>
2023-09-13 14:47:53 -0400

    Added in a photography page

---
+++ pages/photos.tsx
@@ -0,0 +1,11 @@
+import Container from 'components/Container'
+
+export default function Photos() {
+  return (
+    <Container>
+      <div className='flex flex-col items-center justify-center inset-0'>
+        <p>This page is dedicated to photos that I've taken.</p>
+      </div>
+    </Container>
+  )
+}
