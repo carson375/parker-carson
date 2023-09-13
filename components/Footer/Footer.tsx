8912773e9e9eda1c4f03fd6e7070908ea56beb57
carson375 <carson.375@osu.edu>
2023-09-13 14:33:02 -0400

    Changed the landing page

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,14 +1,14 @@
 export default function Footer() {
   return (
     <p className='mx-auto my-16 text-center text-quaternary'>
-      Crafted with care by
+      Developed by
       <a
         className='ml-1 text-secondary hover:text-primary group'
-        href='https://cretu.dev'
+        href='https://www.linkedin.com/in/parker-c-carson/'
         target='_blank'
         rel='noopener noreferrer'
       >
-        Cristian Crețu
+        Parker Carson
         <span className='ml-1 transition-opacity duration-200 ease-in-out opacity-0 group-hover:opacity-100'>
           ↗
         </span>
