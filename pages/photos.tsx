a1460d77c52944a0c8d2cef97f585786e2dfae3e
carson375 <carson.375@osu.edu>
2023-09-14 00:25:12 -0400

    Initial Photography page

--- pages/photos.tsx
+++ pages/photos.tsx
@@ -1,10 +1,12 @@
 import Container from 'components/Container'
+import { Gallery } from '../components/Gallery/Gallery'
 
 export default function Photos() {
   return (
     <Container>
       <div className='flex flex-col items-center justify-center inset-0'>
         <p>This page is dedicated to photos that I've taken.</p>
+        <Gallery />
       </div>
     </Container>
   )
