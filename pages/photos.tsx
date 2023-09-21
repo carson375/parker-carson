e934a6b59068a1fd94f102659577b2ba8b6a3f1a
carson375 <carson.375@osu.edu>
2023-09-21 00:25:07 -0400

    Updated photography page

--- pages/photos.tsx
+++ pages/photos.tsx
@@ -1,12 +1,35 @@
 import Container from 'components/Container'
 import { Gallery } from '../components/Gallery/Gallery'
 
+const images = [
+  '/photography/Bridge.JPG',
+  '/photography/ASundayAfternoonInParis.JPG',
+  '/photography/Eiffel.JPG',
+  '/photography/Eiffel2.JPG',
+  '/photography/LuxemborgGardens.JPG',
+  '/photography/LuxemborgGardens.JPG',
+  '/photography/LuxGardens2.JPG',
+  '/photography/LuxGardensBuilding.JPG',
+  '/photography/LuxGardensBuilding2.JPG',
+  '/photography/LuxGardensEiffel.JPG',
+  '/photography/LuxGardensStatue.JPG',
+  '/photography/NotreDame.JPG',
+  '/photography/NotreDame2.JPG',
+  '/photography/NotreDameSide.JPG',
+  '/photography/Pantheon.JPG',
+  '/photography/PantheonClose.JPG',
+  '/photography/ParisBuilding.JPG',
+  '/photography/Street.JPG',
+]
+
+const images2 = ['/photography/Bridge.JPG']
+
 export default function Photos() {
   return (
     <Container>
       <div className='flex flex-col items-center justify-center inset-0'>
         <p>This page is dedicated to photos that I've taken.</p>
-        <Gallery />
+        <Gallery images={images} />
       </div>
     </Container>
   )
