7e36b398e05f2e530480792e032a5a78aff4d32e
carson375 <carson.375@osu.edu>
2023-09-21 00:25:07 -0400

    Updated photography page

--- components/Gallery/Gallery.js
+++ components/Gallery/Gallery.js
@@ -1,44 +1,25 @@
 import React from 'react'
-import Image from 'next/image'
-import Imgix from 'react-imgix'
-import Bridge from '../../public/photography/Bridge.JPG'
+import ModalImage from 'react-modal-image'
+import { Box, Grid } from '@mui/material'
 
-const images = [
-  '/photography/Bridge.JPG',
-  '/photography/ASundayAfternoonInParis.JPG',
-  '/photography/Eiffel.JPG',
-  '/photography/Eiffel2.JPG',
-  '/photography/LuxemborgGardens.JPG',
-  '/photography/LuxemborgGardens.JPG',
-  '/photography/LuxGardens2.JPG',
-  '/photography/LuxGardensBuilding.JPG',
-  '/photography/LuxGardensBuilding2.JPG',
-  '/photography/LuxGardensEiffel.JPG',
-  '/photography/LuxGardensStatue.JPG',
-  '/photography/NotreDame.JPG',
-  '/photography/NotreDame2.JPG',
-  '/photography/NotreDameSide.JPG',
-  '/photography/Pantheon.JPG',
-  '/photography/PantheonClose.JPG',
-  '/photography/ParisBuilding.JPG',
-  '/photography/Street.JPG',
-]
-
-export const Gallery = () => (
-  <div className='gallery'>
-    {images.map(image => (
-      <Image
-        alt='Next.js Conf photo'
-        className='transform rounded-lg brightness-90 transition will-change-auto group-hover:brightness-110'
-        style={{ transform: 'translate3d(0, 0, 0)' }}
-        src={image}
-        width={720}
-        height={480}
-        sizes='(max-width: 640px) 100vw,
-        (max-width: 1280px) 50vw,
-        (max-width: 1536px) 33vw,
-        25vw'
-      />
-    ))}
-  </div>
+// The problem is this mapping is limited by the fact that the div is only 640 px wide we need to change that
+export const Gallery = ({ images }) => (
+  <Box sx={{ flexGrow: 1 }} p={6} paddingBottom={2}>
+    <Grid item spacing={2}>
+      {images.map((image, index) => (
+        <>
+          <ModalImage
+            small={image}
+            large={image}
+            hideDownload={false}
+            hideZoom={true}
+            style='float:left'
+            key={index}
+            className='transform rounded-lg brightness-90 transition will-change-auto group-hover:brightness-110'
+          />
+          <br />
+        </>
+      ))}
+    </Grid>
+  </Box>
 )
