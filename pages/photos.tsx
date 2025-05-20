f289e917f8291bd56b96a81c2e8093e343643b25
Parker Carson <carson.375@osu.edu>
2025-05-20 12:04:03 -0500

    Made some updates to the photography page

--- pages/photos.tsx
+++ pages/photos.tsx
@@ -4,6 +4,7 @@ import { Gallery } from '../components/Gallery/Gallery'
 
 import photos from '../components/imageOptions.json'
 import { useState } from 'react'
+import { useTheme } from 'next-themes'
 import {
   Box,
   Grid,
@@ -18,6 +19,7 @@ export default function Photos() {
   const [dataIndex, setDataIndex] = useState<number>(0)
   const [photoChoice] = useState(photos)
   const [isRender, setIsRender] = useState(false)
+  const { resolvedTheme } = useTheme()
 
   const onTripChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setDataIndex(Number(e.target.value))
@@ -33,7 +35,17 @@ export default function Photos() {
         <Box sx={{ flexGrow: 1 }} width={200} paddingTop={2} paddingBottom={0}>
           <Grid container spacing={2}>
             <FormControl fullWidth>
-              <InputLabel id='demo-simple-select-label'>Trips</InputLabel>
+              <InputLabel
+                id='demo-simple-select-label'
+                sx={{
+                  color: resolvedTheme === 'dark' ? 'white' : 'black',
+                  '&.Mui-focused': {
+                    color: resolvedTheme === 'dark' ? 'white' : 'black',
+                  },
+                }}
+              >
+                Trips
+              </InputLabel>
               <Select
                 labelId='demo-simple-select-label'
                 className='my-mui-select'
@@ -42,9 +54,47 @@ export default function Photos() {
                 onChange={e =>
                   onTripChange(e as React.ChangeEvent<HTMLInputElement>)
                 }
+                sx={{
+                  color: resolvedTheme === 'dark' ? 'white' : 'black', // text color
+                  backgroundColor: resolvedTheme === 'dark' ? '#222' : '#fff', // background
+                  '.MuiSvgIcon-root': {
+                    color: resolvedTheme === 'dark' ? 'white' : 'black',
+                  }, // arrow color
+                }}
+                MenuProps={{
+                  PaperProps: {
+                    sx: {
+                      backgroundColor:
+                        resolvedTheme === 'dark' ? '#222' : '#fff',
+                      color: resolvedTheme === 'dark' ? 'white' : 'black',
+                    },
+                  },
+                }}
               >
                 {photoChoice.map((item, index) => (
-                  <MenuItem value={index} key={index} defaultValue={index}>
+                  <MenuItem
+                    value={index}
+                    key={index}
+                    defaultValue={index}
+                    sx={{
+                      backgroundColor:
+                        resolvedTheme === 'dark' ? '#222' : '#fff',
+                      color: resolvedTheme === 'dark' ? 'white' : 'black',
+                      '&.Mui-selected': {
+                        backgroundColor:
+                          resolvedTheme === 'dark' ? '#333' : '#eee',
+                        color: resolvedTheme === 'dark' ? 'white' : 'black',
+                      },
+                      '&.Mui-selected:hover': {
+                        backgroundColor:
+                          resolvedTheme === 'dark' ? '#444' : '#ddd',
+                      },
+                      '&:hover': {
+                        backgroundColor:
+                          resolvedTheme === 'dark' ? '#333' : '#f5f5f5',
+                      },
+                    }}
+                  >
                     {item.name}
                   </MenuItem>
                 ))}
