c68027671b19805e880f3d5e80093816de0706db
Parker Carson <carson.375@osu.edu>
2025-05-20 12:04:03 -0500

    Made some updates to the photography page

--- components/Gallery/Gallery.js
+++ components/Gallery/Gallery.js
@@ -6,20 +6,18 @@ import ModalImage from 'react-modal-image'
 // The problem is this mapping is limited by the fact that the div is only 640 px wide we need to change that
 export const Gallery = ({ images }) => (
   <Box sx={{ flexGrow: 1 }} p={6} paddingBottom={2}>
-    <Grid item spacing={2}>
+    <Grid container spacing={2}>
       {images.map((image, index) => (
-        <>
+        <Grid item xs={12} sm={6} md={4} key={index}>
           <ModalImage
             small={image}
             large={image}
             hideDownload={false}
             hideZoom={true}
-            style='float:left'
-            key={index}
             className='transform rounded-lg brightness-90 transition will-change-auto group-hover:brightness-110'
+            style={{ width: '100%', height: 'auto', display: 'block' }}
           />
-          <br />
-        </>
+        </Grid>
       ))}
     </Grid>
   </Box>
